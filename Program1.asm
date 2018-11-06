083142823a50315627112c43c8c654c1402e57a4
TejaThangellaUIC <36115692+TejaThangellaUIC@users.noreply.github.com>
2018-11-06 14:10:24 -0600

    fixing program 1

--- Program1.asm
+++ Program1.asm
@@ -6,29 +6,60 @@ MOVI 1 #R5=1
 SAR 4
 LOAD 5 #R4=MEM[1]=Q
 
+SAR 5 
+MOVI 3 #R5=3
+ADDI 3 #R5=6
+
+
 ##start of main loop
-## have comparison statement for checking if loop is done
-## if not done
-## checks if counter just begun and jumps to setting the current value to six
-## else jumps to multiplication
+MAIN:
+SAR 6
+MOVI 3
+SJL 6 #RP=3
+SAR 3
+BSLT 1 #branches out of the loop
+SAR 6
+MOVI 1
+SJL 6 #RP=1
+SAR 1
+BSLT 6 
+
+
 
 #counter to begin
+MULTI:
 SAR 2
 MOVI 6
-B MOD
+SAR 6
+MOVI 2 #R6=2
+SJL 6  #RP=2
+B 
 
-#multiplication loop
+
+MULT:
 SAR 2
 MLT 5
-B MOD
+SAR 6
+MOVI 2 #R6=2
+SJL 6  #RP=2
+B 
 
 #begining of MOD branch
+MOD:
+SAR 6
+MOVI 3 #R6=3
+SJL 6  #RP=3
+SAR 3
+BSLT 2
 SAR 2
-BSLT #branches out if currentValue is less than Q branches out 
 SUB 4
-B    #branches back to main
+SAR 6
+MOVI 2 #R6=2
+SJL 6  #RP=2
+B    #branches back to beg of MOD
 
 #begining of done branch
+PROGRAMFINISH:
 SAR 1
 MOVI 2
 SAR 2 #R2 IS ACTIVE
