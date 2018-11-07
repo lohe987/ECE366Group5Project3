f2e3edc9ef1523f3e2af8e492ee19fbb866052c0
TejaThangellaUIC <36115692+TejaThangellaUIC@users.noreply.github.com>
2018-11-06 19:18:02 -0600

    probable bug

--- Program1.asm
+++ Program1.asm
@@ -1,20 +1,12 @@
 SAR 3 #R3 IS ACTIVE
 LOAD 0 #R3=P
-
 SAR 5 #R5 IS ACTIVE
 MOVI 1 #R5=1
 SAR 4
 LOAD 5 #R4=MEM[1]=Q
-
 SAR 5 
 MOVI 3 #R5=3
 ADDI 3 #R5=6
-
-SAR 6
-MOVI 0
-BSLT 3 
-
-#start of main loop 0
 MAIN:
 SAR 6
 MOVI 3
@@ -22,58 +14,54 @@ ADDI 1
 SAR 0
 MOVI 0
 SJL 6 #RP=4
-SAR 1
-BSLT 3 #branches out of the loop
+SAR 3
+BSLT 1 
+SAR 3
+BSLT 1 
 SAR 1
 ADDI 1
 SAR 6
 MOVI 2
-SJL 6 #RP=2
+SAR 0
+MOVI 0
+SJL 6 
+SAR 6
 MOVI 1 
-SAR 1
-BSLT 6 
-
-
-
-#counter to begin 1
+SAR 6
+BSLT 1 
 MULTI:
 SAR 2
 MOVI 6
 SAR 6
-MOVI 3 #R6=3
-SJL 6  #RP=3
+MOVI 3 
+SAR 0
+MOVI 0
+SJL 6  
 B 
-
-#2
 MULT: 
 SAR 2
 MLT 5
 SAR 6
-MOVI 3 #R6=3
-SJL 6  #RP=3
+MOVI 3 
+SAR 0
+MOVI 0
+SJL 6  
 B 
-
-#3
-#begining of MOD branch
 MOD:
 SAR 6
-MOVI 0 #R6=0
-SJL 6  #RP=0
-SAR 3
-BSLT 2
+MOVI 0
+SJL 6  
+SAR 2
+BSLT 3
 SAR 2
 SUB 4
 SAR 6
-MOVI 3 #R6=3
-SJL 6  #RP=3
-B    #branches back to beg of MOD
-
-#begining of done branch 4
+MOVI 3 
+SAR 0
+MOVI 0
+SJL 6  
+B    
 PROGRAMFINISH:
 SAR 1
 MOVI 2
-SAR 2 #R2 IS ACTIVE
-STOR 1 #R2=MEM[2]
-DONE
-
 
